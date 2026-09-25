46389fc6eb951e92b18aa917ea55b6be733f0339
Pitchayathida Wannasrimuang <Pitchayathida.w@ku.th>
2026-09-25 15:31:04 +0700

    week07

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -22,3 +22,26 @@ class Rectangle:
 rect = Rectangle(10, 5)
 print(rect.get_area())       # Should print 50
 print(rect.get_perimeter())  # Should print 30
+
+"""
+ขอให้เขียนคราส circle ที่ทำงานคล้ายคลึงกับคราส Rectangle พร้อมตัวอย่างการทำงาน
+"""
+
+class Circle:
+    # ปรับให้ถูกต้องกับความเป็นวงกลม
+    def init(self, length, width):
+        self.length = length
+        self.width = width
+
+    # Method to get the area ปรับสูตรพื้นที่วงกลม
+    def get_area(self):
+        return self.length * self.width
+
+    # Method to get the perimeter ปรับสูตรเส้นรอบวงวงกลม
+    def get_perimeter(self):
+        return f"Perimeter = {self.length} * {self.width} = {2 * (self.length * self.width)}"
+
+
+myCircle = Circle(10)
+print(myCircle.get_area())
+print(myCircle.get_perimeter())
